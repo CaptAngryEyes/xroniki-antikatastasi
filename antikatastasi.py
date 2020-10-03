9ee8a6f8dd0c45bebf6092c671b1bb2988ca364a
Konstantinos Chatzis <69366501+CaptAngryEyes@users.noreply.github.com>
2020-10-03 02:10:01 -0700

    Added letter combination for letters τ, φ

--- antikatastasi.py
+++ antikatastasi.py
@@ -12,16 +12,38 @@ def AskForVerb():
             print('First part = ' + firstpart)
             for k, i in endings.items():
                 if(desiredTense) == k:
-                    #If the firstpart contains a double 'τ' and if the secondpart contains 'σ'
-                    if firstpart[-2:] == 'ττ' and i[:1] == 'σ': # τ + σ
+                    #LETTER COMBINATION
+                    # -START- τ + σ = ξ
+                    if (firstpart[-1:] == 'τ' and i[:1] == 'σ'):
+                        firstpart = firstpart[:-1]    
+                        i = i[1:]
+                        firstpart = firstpart + 'ξ'
+                        newVerb = firstpart + i
+                        print(newVerb)
+                    elif (firstpart[-2:] == 'ττ' and i[:1] == 'σ'):
                         firstpart = firstpart[:-2]    
                         i = i[1:]
-                        firstpart = firstpart + 'ξ'             # = ξ
+                        firstpart = firstpart + 'ξ'
+                        newVerb = firstpart + i
+                        print(newVerb)
+                    # -END- τ + σ = ξ
+                    # -START- φ + σ = ψ
+                    elif (firstpart[-1:] == 'φ' and i[:1] == 'σ'):
+                        firstpart = firstpart[:-1]    
+                        i = i[1:]
+                        firstpart = firstpart + 'ψ'
+                        newVerb = firstpart + i
+                        print(newVerb)
+                    elif (firstpart[-2:] == 'φφ' and i[:1] == 'σ'):
+                        firstpart = firstpart[:-2]    
+                        i = i[1:]
+                        firstpart = firstpart + 'ψ'
                         newVerb = firstpart + i
                         print(newVerb)
                     else:
                         newVerb = firstpart + i
                         print(newVerb)
+                    # -END- φ + σ = ψ
         else:
             print('none')
     else:
